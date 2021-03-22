d242c64f25b6b15a385e35fdd95811f8bdf0ed07
Claudio Henrique da Silva Martins <criarsitecommartins@gmail.com>
2021-03-22 17:40:42 -0300

    Update CredentialsSeeder.php

--- database/seeders/CredentialsSeeder.php
+++ database/seeders/CredentialsSeeder.php
@@ -16,7 +16,7 @@ class CredentialsSeeder extends Seeder
     {
         DB::table('credentials')->insert([
             'name' => "x-access-token",
-            "token"  => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmZWVnb3ciLCJhdWQiOiJwdWJsaWNhcGkiLCJpYXQiOiIxNy0wOC0yMDE4IiwibGljZW5zZUlEIjoiMTA1In0.UnUQPWYchqzASfDpVUVyQY0BBW50tSQQfVilVuvFG38"
+            "token"  => ""
         ]);
     }
 }
